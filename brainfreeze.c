906eede9d848fc6e1f7264b8f4b458e2e096799a
Alexander Langford <nirvashtzero@gmail.com>
2015-11-10 23:02:44 -0500

    Can read files in and display the whole file

--- brainfreeze.c
+++ brainfreeze.c
@@ -1,8 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "brainfreeze.h"
 
+/*
+ * A simple function to print out the entire program.
+ */
+void print_program(FILE *program)
+{
+	char instruction;
+	while (fscanf(program, "%c", &instruction) != EOF) {
+		fprintf(stdout, "%c", instruction);
+	}
+}
+
+/*
+ * Simple function to print the program's usage.
+ */
+void usage()
+{
+	printf("Usage: brainfreeze <prog>\n");
+	printf("<prog> is a program written in Brainfuck\n");
+}
+
+/* 
+ * Main function of the interpreter
+ *
+ * Parsing command line arguments:
+ * Here I opted to just roll my own system.
+ * I might switch to getopts if I do anything more complicated
+ * than just taking in a program. Probably will, honestly.
+ */
 int main(int argc, char** argv)
 {
+	FILE *program;
+	char *filename;
+
+	if (argc < 2) {
+		printf("Error: No filename given\n");
+		usage();
+		exit(1);
+	} else if (3 < argc) {
+		printf("Error: Only expecting program filename\n");
+		usage();
+		exit(1);
+	}
+	filename = argv[1];
+
+	program = fopen(filename, "r");
+	if (!program) {
+		fprintf(stderr, "Error: %s doesn't exist or can't be read\n", filename);
+		exit(1);
+	}
+
 	printf("BrainFreeze v0.1\n");
+	printf("Program loaded: %s\n", filename);
+	print_program(program);
+
+	fclose(program);
 	return 0;
 }
 
---
+++ brainfreeze.h
@@ -0,0 +1,9 @@
+#ifndef _BRAINFREEZE_H
+#define _BRAINFREEZE_H
+
+struct machine_state {
+};
+
+void print_program(FILE *program);
+
+#endif
